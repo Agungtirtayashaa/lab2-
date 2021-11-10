bdc9548307d4aae4887c7be2a6ac25386ebccde9
agung tirta yasha <agungtirta599@gmail.com>
2021-11-10 20:15:10 +0700

    menambahkan file web1.py

---
+++ web1.py
@@ -0,0 +1,22 @@
+a = int(input('Masukkan nilai a: '))
+b = int(input('Masukkan nilai b: '))
+
+if a > b  :
+  print('A yang terbesar')
+elif b > a :
+  print('B yang terbesar')
+
+
+
+print (" program mengurutkan data")
+
+bilanganke1 = [23,54,33,75]
+bilanganke2 = [12,34,21,31]
+bilanganke3 = [ 1,4,2,7,5]
+
+bilanganke1.sort()
+bilanganke2.sort()
+bilanganke3.sort()
+print(bilanganke1)
+print(bilanganke2)
+print(bilanganke3)
